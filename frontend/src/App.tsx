2d9d7fac3e450f550dfa4aa9a10ef87ef4164bf9
Tem Tamre <temtamre@gmail.com>
2025-06-30 20:35:02 -0600

    added frontend skeleton + validators + better handlers + cleaner db operations

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './kalendar.png';
+import './App.css';
+import './skeleton.css';
+import './normalize.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
